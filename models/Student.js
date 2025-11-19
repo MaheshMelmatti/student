e021e7f735e15d5219e6df95b2ed25f84b5679f8
MaheshMelmatti <maheshmelmatti06@gmail.com>
2025-11-19 10:51:33 +0530

    initial commit

---
+++ models/Student.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const studentSchema = new mongoose.Schema({
+  name: { type: String, required: true, trim: true },
+  email: { type: String, required: true, trim: true, lowercase: true },
+  course: { type: String, default: '' },
+  roll: { type: String, default: '' }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Student', studentSchema);
